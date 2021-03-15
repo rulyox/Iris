5afc28de77b4034af2225887df0f708cde1f4c3b
rulyox <email@rulyox.com>
2021-03-15 14:20:39 +0900

    Added end server to network leave

--- src/api/commands/networkLeave.ts
+++ src/api/commands/networkLeave.ts
@@ -6,7 +6,9 @@ const networkLeave = (): CommandResult => {
 
     if(!state.isConnected) return new CommandResult(false, 'Not connected to a network');
 
+    socket.server.endServer();
     socket.client.endClient();
+
     state.isConnected = false;
 
     return new CommandResult(true, null);
