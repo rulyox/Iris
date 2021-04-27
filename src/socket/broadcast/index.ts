8f74756dfd764f6f04ec8b4c99ed758367ccb165
rulyox <email@rulyox.com>
2021-04-27 11:01:59 +0900

    Added print broadcast

--- src/socket/broadcast/index.ts
+++ src/socket/broadcast/index.ts
@@ -2,12 +2,15 @@ import fs from 'fs';
 import { SocketStream } from 'stream-socket.io';
 import { getTargetSockets } from './target';
 import { fileEvent } from '../event';
+import { print } from '../../utility';
 
 const socketStream = new SocketStream();
 
 const broadcastFile = (path: string, name: string, targetSockets: any) => {
 
     const sockets: any[] = getTargetSockets(targetSockets);
+
+    print('job', `Broadcasting file : ${name}`);
 
     for(const socket of sockets) {
 
