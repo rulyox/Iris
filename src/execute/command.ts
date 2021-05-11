db5f9b03cc24b8e58e97e65353630f6796bde61c
rulyox <email@rulyox.com>
2021-05-11 15:50:58 +0900

    Changed docker command options

--- src/execute/command.ts
+++ src/execute/command.ts
@@ -2,10 +2,10 @@ export const dockerLoad = (imagePath: string) =>
     `docker load --input ${imagePath}`;
 
 export const dockerRun = (imageName: string, containerName: string) =>
-    `docker run --interactive --tty --detach --volume /workspace:/workspace --name ${containerName} ${imageName}`;
+    `docker run --detach --volume /workspace:/workspace --name ${containerName} ${imageName}`;
 
 export const dockerExec = (containerName: string, command: string) =>
-    `docker exec --interactive --tty ${containerName} ${command}`;
+    `docker exec --interactive ${containerName} ${command}`;
 
 export const dockerRm = (containerName: string) =>
     `docker rm --force ${containerName}`;
